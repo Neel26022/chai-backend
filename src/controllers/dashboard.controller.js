14b5e080b08549386e1e6b413247da893f6e99c1
Neel Prajapati <neelprajapati260@gmail.com>
2024-05-29 06:54:03 -0700

    Add dashboard controller not add getChannelStats

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,10 +8,25 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    
+
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+
+    const { ownerId } = req.params
+
+    const videos = await Video.find({ owner: ownerId });
+
+    if (!videos) {
+        return res.status(200).json(
+            new ApiResponse(200, [], "No videos found for this channel")
+        );
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, videos, "Videos fetched successfully")
+    );
 })
 
 export {
